43c5d1959632dc6d165c2f4d70a0551bf121eefe
yake <mr_coder@163.com>
2023-07-25 12:32:47 +0800

    chore(*):update run.bat

--- run.bat
+++ run.bat
@@ -1,24 +1,86 @@
 @echo off
-setlocal enabledelayedexpansion
 
-:FindPythonCommand
-for %%A in (python python3) do (
-    where /Q %%A
-    if !errorlevel! EQU 0 (
-        set "PYTHON_CMD=%%A"
-        goto :Found
-    )
-)
+call :get_python3
 
-echo Python not found. Please install Python.
-pause
-exit /B 1
-
-:Found
-%PYTHON_CMD% scripts/check_requirements.py requirements.txt
 if errorlevel 1 (
-    echo Installing missing packages...
-    %PYTHON_CMD% -m pip install -r requirements.txt
+    echo Python 3.7 or higher is required to run DevOpsGPT.
+    exit /b 1
 )
-%PYTHON_CMD% backend/run.py  %*
-pause
+
+echo Installing missing packages...
+call %PYTHON_CMD% -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
+
+start "" %PYTHON_CMD% backend\run.py
+
+call :start_frontend %PYTHON_CMD%
+
+call :kill_by_port %%BACKEND_PORT%%
+
+exit /b 0
+
+REM function
+:get_python3
+    for /f "delims=" %%i in ('where python 2^>nul') do (
+        set "PYTHON_CMD=%%i"
+        goto :pyCheck
+    )
+    :pyCheck
+    if not defined PYTHON_CMD (
+        echo Python3 not found. Please install Python3.7 or higher.
+        exit /b 1
+    )
+GOTO:EOF
+
+:get_config_value
+    set "yaml_file=env.yaml"
+    set "key=%~1"
+
+    for /f "usebackq tokens=2 delims=: " %%a in (`findstr /C:"%key%:" "%yaml_file%"`) do (
+        set "value=%%a"
+    )
+
+    if not defined value (
+        echo Error: Key '%key%' not found in config file '%yaml_file%'.
+        exit /b 1
+    )
+    set "%key%=!value!"
+GOTO:EOF
+
+:start_frontend
+    set "PYTHON_CMD=%~1"
+    call :get_config_value FRONTEND_PORT
+    call :get_config_value BACKEND_PORT
+
+    REM Wait for the backend service to start
+    for /l %%i in (1, 1, 15) do (
+        set "response="
+        for /f "delims=" %%a in ('curl -s -o nul -w "%%{http_code}" http://127.0.0.1:%BACKEND_PORT%') do set "response=%%a"
+
+        echo !response!
+        if "!response!"=="404" (
+            echo.
+            echo Service started successfully, please use a browser to visit: http://127.0.0.1:!FRONTEND_PORT!
+            GOTO :starFrontend
+        ) else (
+            echo Service has not started yet, please wait...
+            timeout /t 10 > nul
+        )
+    )
+    :starFrontend
+    call %PYTHON_CMD% -m http.server %FRONTEND_PORT% --directory frontend
+GOTO:EOF
+
+:kill_by_port
+    set "port=%~1"
+    for /f "delims=" %%a in ('netstat -aon ^| findstr "LISTENING" ^| findstr ":!port!"') do (
+        set "line=%%a"
+        for /f "tokens=5" %%b in ("!line!") do set "pid=%%b"
+    )
+
+    if not defined pid (
+        echo The port is not in use: %port%
+        exit /b 1
+    )
+
+    taskkill /f /pid %pid%
+GOTO:EOF
