be3207b9f79b59fac0222886292812962178c631
booboosui <suixiaobo@kuafuai.net>
2024-04-28 12:08:37 +0800

    chore(llm): 减少重试次数

--- backend/app/pkgs/tools/llm.py
+++ backend/app/pkgs/tools/llm.py
@@ -23,22 +23,7 @@ def chatCompletion(context, fackData="", bill: bool = True):
             try:
                 message, total_tokens, success = obj.chatCompletion(context, fackData, bill)
             except Exception as e:
-                print("chatCompletion failed 3 time:" + str(e))
+                print("chatCompletion failed 2 time:" + str(e))
                 traceback.print_exc()
-                try:
-                    message, total_tokens, success = obj.chatCompletion(context, fackData, bill)
-                except Exception as e:
-                    print("chatCompletion failed 4 time:" + str(e))
-                    traceback.print_exc()
-                    try:
-                        message, total_tokens, success = obj.chatCompletion(context, fackData, bill)
-                    except Exception as e:
-                        print("chatCompletion failed 5 time:" + str(e))
-                        traceback.print_exc()
-                        try:
-                            message, total_tokens, success = obj.chatCompletion(context, fackData, bill)
-                        except Exception as e:
-                            print("chatCompletion failed 6 time:" + str(e))
-                            traceback.print_exc()
-                            raise Exception("服务异常，请重试。Service exception, please try again.")
+                raise Exception("服务异常，请重试。Service exception, please try again.")
     return message, total_tokens, success
